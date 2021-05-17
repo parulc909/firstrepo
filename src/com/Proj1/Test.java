23eb7f04011dfe798cbe3614a13e569ccc796e7d
unknown <parulc909@gmail.com>
2021-05-17 21:03:25 +0800

    newly added to vsscode branch

--- src/com/Proj1/Test.java
+++ src/com/Proj1/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 System.out.println("this is first class");
 System.out.println("updating code again");
+System.out.println("in vsscode branch");
 	}
 
 }
